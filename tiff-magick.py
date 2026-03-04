ae8e7d5bb951bc092a5ecce6c472958ac59b1e5c
Jonathan Candelaria <candelariajr@appstate.edu>
2026-03-04 16:25:34 -0500

    Create tiff-magick.py

---
+++ tiff-magick.py
@@ -0,0 +1,53 @@
+import subprocess
+import os
+
+# Check to see if Python is awake in terminal
+print("Welcome to Jonathan Candelaria's PDF to TIFF Magick!")
+
+# Run test to see if command line works for ImageMagick and that magick is working
+subprocess.run([
+    "magick",
+    "-size", "300x100",
+    "xc:black",
+    "-fill", "white",
+    "-gravity", "center",
+    "-pointsize", "24",
+    "-annotate", "0", "Hello",
+    "test.png"
+])
+
+# If we haven't crashed, delete the test file.
+if os.path.exists("test.png"):
+    os.remove("test.png")
+    print("Functionality Test Complete")
+else:
+    print("Failed to test Magick. Please check your magick installation and try again.")
+    print(quit)
+
+
+# Process to run for an individual file
+def process_pdf(file_name):
+    print(file_name)
+    result = subprocess.run(
+        # This is a string locator test
+        # ["magick", "identify", "\"" + file_name + "\""],
+        ["magick", "identify", file_name],
+        capture_output=True,
+        text=True
+    )
+    pages = int(len(result.stdout.splitlines()))
+    for x in range(pages):
+        process_page((x + 1), file_name)
+
+
+# Process for an individual page
+def process_page(number, file_name):
+    print(str(number) + " " + file_name)
+
+
+# Loop Through Input Files
+folder = "input-files"
+for file in os.listdir(folder):
+    if file.lower().endswith(".pdf"):
+        pdf_path = os.path.join(folder, file)
+        process_pdf(pdf_path)
